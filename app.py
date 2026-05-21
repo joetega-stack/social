c46bfb499f9ea7b4b34bc27ad816dd9def0e42b6
gill <gillbradford59@gmail.com>
2026-05-21 16:45:39 +0100

    remove sqlalchemy local db

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 from fastapi import FastAPI,Response
-from typing import Optional
-from pydantic import BaseModel
-from lib.database import engine
 from routes import userRoutes,authRoutes,postRoutes
-from models import Base
 from fastapi.middleware.cors import CORSMiddleware
 from supabase_client import supabase
 
 
 
-Base.metadata.create_all(bind=engine)
 # Base.metadata.drop_all(bind=engine)
 
 server = FastAPI()
